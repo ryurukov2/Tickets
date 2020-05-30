b98b17e72f4157fcda4b4eacbb0d8eb541760fbf
Radoslav <idarino@live.com>
2020-05-30 10:01:42 +0300

    asd

--- Booking_Events.cpp
+++ Booking_Events.cpp
@@ -1,11 +1,13 @@
 #include<iostream>
+#include<math.h>
+#include<iomanip>
+#include<time.h>
 #include<string>
 #include<fstream>
-
-
-
 #define capacity "R10000000 R20000000 R30000000 R40000000 R50000000"
 using namespace std;
+
+
 class EventMangement
 {
 
@@ -21,7 +23,7 @@ public:
 		string date = "";
 		string hall = "";
 		string event_name = "";
-		cout << "Give me the date(in yyyy/mm/dd format) and hall and the name of the event to be created" << endl;
+		cout << "Give me the date(in dd/mm/yy format) and hall and the name of the event to be created" << endl;
 		cin >> date;
 		cin >> hall;
 		cin >> event_name;
@@ -90,14 +92,14 @@ public:
 	void freeseats()
 	{
 		string date, name;
-		cout << "Give the correct name and date(format yyyy/mm/dd) of the event to check if either available in the system\n";
+		cout << "Give the correct name and date(format dd/mm/yy) of the event to check if either available in the system\n";
 		cin >> name;
 		cin >> date;
 		fstream check("BookTickets.txt");
 		bool found = false;
 		if (!check.good())
 		{
-			cout << "There arent any events.\n";
+			cout << "There is not a single event in the system to add an event use addevent option\n";
 			check.close();
 		}
 		else
@@ -133,6 +135,385 @@ public:
 			if (found == false)
 			{
 				cout << "There is no such event occuring on that date.\n";
+			}
+		}
+	}
+	void book()
+	{
+		string rows, seat, date, name;
+		cout << "Give the correct name and date(format dd/mm/yy) of the event to check if either available in the system\n";
+		cin >> name;
+		cin >> date;
+		string lines[100] = { "" };
+		string bookinginfo = "";
+		fstream check("BookTickets.txt", fstream::in);
+		int i = 0;
+		bool found = false;
+		if (!check.good())
+		{
+			cout << "There is not a single event in the system to add an event use addevent option\n";
+			check.close();
+		}
+		else
+		{
+			while (!check.eof())
+			{
+				string line = "";
+				getline(check, line);
+				string check_hall = line;
+				string del = ",";
+				string event_name = check_hall.substr(0, check_hall.find(","));
+				check_hall.erase(0, check_hall.find(del) + 1);
+				string event_date = check_hall.substr(0, check_hall.find(","));
+				if (name == event_name && date == event_date)
+				{
+					string row = "";
+					string seat = "";
+					cout << "Choose row number from 1-5" << endl;
+					cin >> row;
+					if (stoi(row) < 0 || stoi(row) > 5)
+					{
+						cout << "This row doesn't exist";
+						return;
+
+					}
+
+					else
+					{
+						found = true;
+						cout << "Choose seat number from 0-6\n";
+						cin >> seat;
+						if (stoi(seat) < 0 || stoi(seat) > 6)
+						{
+							cout << "Wrong Seat Number";
+							return;
+						}
+						/*	int startingseat = line.find("R" + row)+2;
+							int lastseat = line.find(line.find("R" + row), line.find(" "));	*/
+						string row_number = "R" + row;
+
+						string whole_row = line.substr(line.find(row_number) + 2, 8);
+						if (whole_row[stoi(seat)] == '1')
+						{
+							cout << "Sorry already booked or bought\n";
+						}
+						else if (whole_row[stoi(seat)] == '0')
+						{
+							whole_row[stoi(seat)] = '1';
+							cout << "Ticket Booked\n";
+							string new_row = row_number + whole_row;
+							line.replace(line.find(row_number), new_row.length(), new_row);
+							bookinginfo = "Row" + row + "," + "Seat" + seat + "," + name + "," + date + "," + "TICKET BOOKED UNTILL TOMORROW";
+						}
+					}
+
+
+
+				}
+				lines[i] = line;
+				i++;
+			}
+			check.close();
+
+
+			if (found == false)
+			{
+				cout << "There is no such event.";
+			}
+			else
+			{
+				int count = 0;
+				if (lines[count] != "")
+				{
+					ofstream out("Booktickets.txt");
+					while (lines[count] != "")
+					{
+						out << lines[count] << endl;
+						count++;
+					}
+					out.close();
+				}
+				if (bookinginfo != "")
+				{
+					fstream book("BookedTickets.txt", fstream::out | fstream::app);
+					book << bookinginfo << endl;
+					book.close();
+				}
+			}
+		}
+	}
+	void unbook()
+	{
+
+		string rows, seat, date, name;
+		cout << "Give the correct name and date(format dd/mm/yy) of the event to Unbook your ticket\n";
+		cin >> name;
+		cin >> date;
+		string lines[100] = { "" };
+		string booked[100] = { "" };
+		fstream check("BookTickets.txt", fstream::in);
+		fstream tickets("BookedTickets.txt", fstream::in);
+		ofstream ostrm("BookedTickets1.txt");
+		int i = 0;
+		int count = 0;
+		bool found = false;
+		if (!tickets.good())
+		{
+			cout << "There are no booked tickets yet." << endl;
+			tickets.close();
+			check.close();
+			return;
+		}
+		else
+		{
+			string row, seat;
+			cout << "Row number from 1-5" << endl;
+			cin >> row;
+			if (stoi(row) < 1 || stoi(row) > 5)
+			{
+				cout << "Wrong Row number , this doesn't exist";
+				return;
+
+			}
+			cout << "Seat number from 0-6\n";
+			cin >> seat;
+			if (stoi(seat) < 0 || stoi(row) > 6)
+			{
+				cout << "Wrong Seat number , this doesn't exist";
+				return;
+			}
+
+			while (!check.eof())
+			{
+				string line = "";
+				getline(check, line);
+				string check_hall = line;
+				string del = ",";
+				string event_name = check_hall.substr(0, check_hall.find(","));
+				check_hall.erase(0, check_hall.find(del) + 1);
+				string event_date = check_hall.substr(0, check_hall.find(","));
+				if (name == event_name && date == event_date)
+				{
+
+					found = true;
+
+					/*	int startingseat = line.find("R" + row)+2;
+					int lastseat = line.find(line.find("R" + row), line.find(" "));	*/
+					string row_number = "R" + row;
+
+					string whole_row = line.substr(line.find(row_number) + 2, 8);
+					if (whole_row[stoi(seat)] == '0')
+					{
+						cout << "Seat not unbooked\n";
+					}
+					else if (whole_row[stoi(seat)] == '1')
+					{
+						whole_row[stoi(seat)] = '0';
+						cout << "Ticket Unbooked\n";
+						string new_row = row_number + whole_row;
+						line.replace(line.find(row_number), new_row.length(), new_row);
+
+					}
+
+
+
+				}
+				lines[i] = line;
+				i++;
+
+			}
+			string res_row = "Row" + row;
+			string res_seat = "Seat" + seat;
+			while (!tickets.eof())
+			{
+
+				string tick;
+				getline(tickets, tick);
+				string del = ",";
+				string chek_tick = tick;
+				string check_row = tick.substr(0, tick.find(","));
+				chek_tick.erase(0, chek_tick.find(del) + del.length());
+				string check_seat = chek_tick.substr(0, chek_tick.find(","));
+				if (res_row != check_row || res_seat != check_seat)
+				{
+					ostrm << tick << endl;
+					booked[count] = tick;
+					count++;
+				}
+			}
+			check.close();
+			tickets.close();
+			ostrm.close();
+			if (remove("BookedTickets.txt") != 0) {
+				cout << "Cant delete file." << endl;
+			}
+			if (rename("BookedTickets1.txt", "BookedTickets.txt") != 0) {
+				cout << "Cant rename files." << endl;
+			}
+			if (found == false)
+			{
+				cout << "No such event ever made in the system";
+				return;
+			}
+			else
+			{
+
+
+				int count = 0;
+				if (lines[count] != "")
+				{
+					ofstream out("BookTickets.txt");
+					while (lines[count] != "")
+					{
+						out << lines[count] << endl;
+						count++;
+					}
+					out.close();
+				}
+
+				count = 0;
+				if (booked[count] != "")
+				{
+					ofstream tickets("BookedTickets.txt");
+					while (booked[count] != "")
+					{
+						tickets << booked[count] << endl;
+						count++;
+					}
+					tickets.close();
+				}
+			}
+		}
+
+	}
+	void buy()
+	{
+		string row, seat, date, name;
+		cout << "Give the correct name and date(format dd/mm/yy) of the event to Buy those tickets\n";
+		cin >> name;
+		cin >> date;
+
+		string lines[100] = { "" };
+		string soldticket = "";
+		fstream check("BookTickets.txt");
+		ifstream ifstrm("BookedTickets.txt");
+		ofstream ostrm("BookedTickets1.txt");
+		int i = 0;
+		bool found = false;
+		if (!check.good())
+		{
+			cout << "There is not a single event in the system to add an event use addevent option\n";
+			check.close();
+		}
+		else
+		{
+			while (!check.eof())
+			{
+				string line = "";
+				getline(check, line);
+				string check_hall = line;
+				string del = ",";
+				string event_name = check_hall.substr(0, check_hall.find(","));
+				check_hall.erase(0, check_hall.find(del) + 1);
+				string event_date = check_hall.substr(0, check_hall.find(","));
+				if (name == event_name && date == event_date)
+				{
+
+
+					cout << "Рow number from 1-5" << endl;
+					cin >> row;
+					if (stoi(row) < 0 || stoi(row) > 5)
+					{
+						cout << "Wrong Row number , this doesn't exist";
+						return;
+					}
+
+					else
+					{
+						found = true;
+						cout << "Give me the Seat number from 0-6\n";
+						cin >> seat;
+						if (stoi(seat) < 0 || stoi(seat) > 6)
+						{
+							cout << "Wrong Seat number , this doesn't exist";
+							return;
+						}
+						/*	int startingseat = line.find("R" + row)+2;
+						int lastseat = line.find(line.find("R" + row), line.find(" "));	*/
+						string row_number = "R" + row;
+
+						string whole_row = line.substr(line.find(row_number) + 2, 8);
+						if (whole_row[stoi(seat)] == '0')
+						{
+							cout << "Sorry First Book your Ticket\n";
+						}
+						else if (whole_row[stoi(seat)] == '1')
+						{
+							whole_row[stoi(seat)] = '2';
+							cout << "Ticket Sold\n";
+							string new_row = row_number + whole_row;
+							line.replace(line.find(row_number), new_row.length(), new_row);
+							soldticket = "R" + row + "," + "S" + seat + "," + name + "," + date + "\n";
+						}
+
+					}
+
+				}
+				lines[i] = line;
+				i++;
+
+			}
+			string res_row = "Row" + row;
+			string res_seat = "Seat" + seat;
+			while (!ifstrm.eof()) {
+				string tick;
+				getline(ifstrm, tick);
+				string del = ",";
+				string chek_tick = tick;
+				string check_row = tick.substr(0, tick.find(","));
+				chek_tick.erase(0, chek_tick.find(del) + del.length());
+				string check_seat = chek_tick.substr(0, chek_tick.find(","));
+				if (res_row != check_row || res_seat != check_seat)
+				{
+					ostrm << tick << endl;
+				}
+			}
+
+			check.close();
+			ostrm.close();
+			ifstrm.close();
+			if (remove("BookedTickets.txt") != 0) {
+				cout << "Cant delete file." << endl;
+			}
+			if (rename("BookedTickets1.txt", "BookedTickets.txt") != 0) {
+				cout << "Cant rename files." << endl;
+			}
+			if (found == false)
+			{
+				cout << "No such event" << endl;
+			}
+			else
+			{
+
+				if (soldticket != "")
+				{
+					fstream buy("SOLD_TICKETS.txt", fstream::out | fstream::app);
+					buy << soldticket;
+					buy.close();
+				}
+
+				int count = 0;
+				if (lines[count] != "")
+				{
+					ofstream update("BookTickets.txt");
+
+					while (lines[count] != "")
+					{
+						update << lines[count] << endl;
+						count++;
+					}
+					update.close();
+				}
 			}
 		}
 	}
--- Tickets.cpp
+++ Tickets.cpp
@@ -1,15 +1,199 @@
 #include<iostream>
+#include<sstream>
+#include "Booking_Events.cpp"
+#include<algorithm>
+#include<stdio.h>
+#include<string>
+#include<stdlib.h>
+using namespace std;
+string lines[100];
+bool compareStr(string str, const char* ch) {
+	transform(str.begin(), str.end(), str.begin(), ::tolower);
+	const char* compArr = str.c_str();
+	return strcmp(ch, compArr);
+}
+string removeFirstWord(string str) {
+	return str.substr(str.find_first_of(" \t") + 1);
+}
+string firstWord(string str)
+{
+	string saveAs = "save as";
+	string word = "", word2 = "";
+	for (auto x : str)
+	{
+		if (x == ' ' || x == '*')
+		{
+			if (compareStr(word, "save") == 0) {
+				string secondW = removeFirstWord(str);
+				for (char y : secondW)
+				{
+					if (y == ' ')
+					{
+						if (compareStr(word2, "as") == 0) {
+							return saveAs;
+						}
+					}
+					else
+					{
+						word2 = word2 + y;
 
+					}
+				}
+			}
+			if (compareStr(word2, "as") == 0) {
+				return saveAs;
+			}
+			else {
+				return word;
+
+			}
+		}
+		else
+		{
+			word = word + x;
+		}
+	}
+	return word;
+}
+
+int strToInt(string str) {
+	int result = 0;
+	for (auto x : str) {
+		if (x == '*') {
+			break;
+		}
+		else if (x >= '0' && x <= '9') {
+			result = 10 * result + (x - '0');
+		}
+		else {
+			return NULL;
+		}
+	}
+	return result;
+}
+void openF(string str) {
+	str = removeFirstWord(str);
+	cout << endl << str << endl;
+	string fileName = firstWord(str);
+	fstream myS;
+	cout << fileName << endl;
+	string line;
+	int i = 0;
+	myS.open(fileName);
+	while (!myS.eof()) {
+		getline(myS, line);
+		lines[i] = line;
+		cout << i + 1 << ". " << lines[i] << endl;
+		i++;
+	}
+	myS.close();
+	cout << "1. remove a line\n 2. change a line\n 3. insert a line\n Anything else = exit open function.\n";
+	string inp;
+	int addcounter = 0;
+	do {
+		getline(cin, inp);
+		int ins = strToInt(inp);
+		if (ins == 1) {
+			int nrLine;
+			cout << "Enter the number of the line you want to remove.\n";
+			cin >> nrLine;
+			lines[nrLine - 1] = "";
+		}
+		else if (ins == 2) {
+			int nrLine;
+			cout << "Enter the number of the line you want to change.\n";
+			cin >> nrLine;
+			string change;
+			cout << "Type the new line.\n";
+			cin.ignore();
+			getline(cin, change);
+			cout << change << endl;
+			lines[nrLine - 1] = change;
+		}
+		else if (ins == 3) {
+			addcounter++;
+			cout << "Enter the number of the line you want to insert after.\n";
+			int nrLine;
+			cin >> nrLine;
+			if (nrLine > i + 1) {
+				return;
+			}
+			string addline;
+			cout << "Enter your line: ";
+			cin.ignore();
+			getline(cin, addline);
+			cout << "asd " << addline << " asd" << endl;
+			for (int j = 0; j < i - nrLine + 1; j++) {
+				lines[i - j + 1] = lines[i - j];
+				cout << lines[j];
+			}
+			lines[nrLine] = addline;
+		}
+		else if (compareStr(firstWord(inp), "close") == 0) {
+			for (int o = 0; o < 100; o++) {
+				lines[o] = "";
+				system("CLS");
+				cout << "\nUse open again to access files.\n";
+				return;
+			}
+		}
+		else if (compareStr(firstWord(inp), "save") == 0) {
+			myS.open("temp.txt", ios::out | ios::app);
+			for (int k = 0; k < i + addcounter; k++) {
+				int l = 0;
+				if (lines[k] == "") {
+					l++;
+					continue;
+				}
+				lines[k - l] = lines[k];
+				myS << lines[k] << endl;
+			}
+			myS.close();
+			if (remove(fileName.c_str()) != 0) {
+				cout << "Can't remove file.\n";
+			}
+			if (rename("temp.txt", fileName.c_str()) != 0) {
+				cout << "Can't rename file.\n";
+
+			}
+		}
+		else if (compareStr(firstWord(inp), "save as") == 0) {
+			cout << "\n save as \n";
+			string inp1 = removeFirstWord(removeFirstWord(inp));
+			myS.open(inp1, ios::out | ios::app);
+			for (int k = 0; k < +addcounter; k++) {
+				int l = 0;
+				if (lines[k] == "") {
+					l++;
+					continue;
+				}
+				lines[k - l] = lines[k];
+				myS << lines[k] << endl;
+			}
+			myS.close();
+		}
+		else if (compareStr(firstWord(inp), "help") == 0) {
+			system("CLS");
+			cout << "\n open <file>	opens <file> \nclose			closes currently opened file \nsave			saves the currently open file\nsave as <file>	saves the currently open file in <file> \nhelp			prints this information";
+		}
+		else if (compareStr(firstWord(inp), "exit") == 0) {
+			exit(0);
+		}
+	} while (true);
+
+}
 int main()
 {
+	EventMangement em;
 
 	while (1)
 	{
-		cout << "CHOOSE THE OPTION YOU WANT";
+		cout << "CHOOSE THE OPTION YOU WANT\n";
 		cout << "1.ADD AN EVENT\n" << "2.CHECK FREE SEATS\n" << "3.BOOK A SEAT\n" << "4.UNBOOK A SEAT\n" << "5.BUY SEATS\n";
-		cout << "6.CHECK ALL BOOKINGS\n" << "7.SHOW REPORT\n";
-		int option = 0;
-		std::cin >> option;
+		cout << "6.CHECK ALL BOOKINGS\n" << "7.SHOW REPORT\n" << "8. EXIT\n";
+		string input;
+		getline(cin, input);
+		int option = strToInt(input);
 		if (option == 1)
 		{
 			em.addevent();
@@ -33,14 +217,20 @@ int main()
 		}
 		else if (option == 6)
 		{
-			em.bookings();
+			//em.bookings();
 		}
 		else if (option == 7)
 		{
-			em.report();
+			//em.report();
 		}
 		else if (option == 8) {
 			break;
+		}
+		else if (compareStr(firstWord(input), "open") == 0) {
+			openF(input);
+		}
+		else if (input == "exit") {
+			exit(0);
 		}
 		else {
 			cout << "Invalid input. Press 1-8.\n";
